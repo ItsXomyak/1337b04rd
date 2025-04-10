2638cee6c4f934576cd5868b934e73576d3e4f88
Azamat Zhaxylykov <a19732805@gmail.com>
2025-04-10 16:32:22 +0500

    refactor: reorganize project structure and remove unused domain repositories

--- internal/domain/comment/repository.go
+++
@@ -1,9 +0,0 @@
-package comment
-
-type ComRepository interface {
-	Create(comment *Comment) (int64, error)
-	GetByID(id int64) (*Comment, error)
-	ListByPost(postID int64) ([]*Comment, error)
-	Update(comment *Comment) error
-	Delete(id int64) error
-}
