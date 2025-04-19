e03de5facff9710c515d3fcecc9df645beedca59
scrameee <93728312+ItsXomyak@users.noreply.github.com>
2025-04-19 16:36:44 +0500

    fix: session_service.go

--- internal/app/services/session_service.go
+++ internal/app/services/session_service.go
@@ -90,7 +90,6 @@ func (s *SessionService) DeleteExpired(ctx context.Context) error {
 }
 
 func (s *SessionService) UpdateDisplayName(ctx context.Context, id utils.UUID, newName string) error {
-	ё
 	err := s.repo.UpdateDisplayName(ctx, id.String(), newName)
 	if err != nil {
 		logger.Error("failed to update display name", "id", id.String(), "error", err)
